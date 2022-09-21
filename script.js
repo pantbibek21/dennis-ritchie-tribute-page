fa341db4152df79c56c2815b7c28e2fb296a1ccd
Bibek Pant <pantbibek21@gmail.com>
2022-09-21 21:31:38 +0545

    index.html and script.js are optimized and comments added

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+// Accessing the elements
+
 const quotes = document.querySelectorAll(".slider-wrapper .img-wrapper");
 const nextBtn = document.querySelector(".fa-arrow-right-long");
 const prevBtn = document.querySelector(".fa-arrow-left-long");
+
+//setting array index 
+
 let index = 0;
+
+//adding event listener to next Button
+
 nextBtn.addEventListener("click", () => {
     window.clearInterval(timeoutHandler);
     nextQuotesGenerator();
@@ -9,25 +17,35 @@ nextBtn.addEventListener("click", () => {
         nextQuotesGenerator();
     }, 4000);
 });
+
+//adding event listener to previous button
 prevBtn.addEventListener("click", previousQuoteGenerator);
 
-
+//function that changes the next slides
 
 function nextQuotesGenerator() {
     activeStatusRemover();
     index++;
     activeStatusAdder();
 }
+
+//function that changes the previous slides
+
 function previousQuoteGenerator() {
     activeStatusRemover();
     index--;
     activeStatusAdder();
 }
+
+//function that removes the active class on slider items 
+
 function activeStatusRemover() {
     quotes.forEach((quote) => {
         quote.classList.remove("active");
     })
 }
+
+// function that adds the active sive status to sliders items
 
 function activeStatusAdder() {
     if (index < quotes.length) {
@@ -39,6 +57,7 @@ function activeStatusAdder() {
     }
 }
 
+// This functions automatically changes the sliders items after 4 seconds
 
 let timeoutHandler = setInterval(() => {
     nextQuotesGenerator();
